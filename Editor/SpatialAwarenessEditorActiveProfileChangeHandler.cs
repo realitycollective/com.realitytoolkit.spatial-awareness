5181b20078712159eb861449d69c764124f10c56
Dino Fejzagić <dino.f@live.de>
2024-05-15 17:30:15 +0200

    Transfer existng code

---
+++ Editor/SpatialAwarenessEditorActiveProfileChangeHandler.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information. 
+
+using RealityCollective.ServiceFramework.Services;
+using RealityCollective.Utilities.Editor;
+using RealityToolkit.SpatialAwareness.Definitions;
+using UnityEditor;
+using UnityEngine;
+
+namespace RealityToolkit.SpatialAwareness.Editor
+{
+    [InitializeOnLoad]
+    public static class EditorActiveProfileChangeHandler
+    {
+        static EditorActiveProfileChangeHandler()
+        {
+            EditorApplication.hierarchyChanged += EditorApplication_hierarchyChanged;
+        }
+
+        private static void EditorApplication_hierarchyChanged()
+        {
+            if (ServiceManager.IsActiveAndInitialized)
+            {
+                if (ServiceManager.Instance.TryGetService<ISpatialAwarenessService>(out _) &&
+                    LayerUtilities.CheckLayers(SpatialAwarenessSystemProfile.SpatialAwarenessLayers))
+                {
+                    Debug.Log($"{nameof(ISpatialAwarenessService)} was enabled, spatial mapping layers added to project.");
+                }
+                else if (!ServiceManager.Instance.TryGetService<ISpatialAwarenessService>(out _) &&
+                         LayerUtilities.RemoveLayers(SpatialAwarenessSystemProfile.SpatialAwarenessLayers))
+                {
+                    Debug.Log($"{nameof(ISpatialAwarenessService)} was disabled, spatial mapping layers removed to project.");
+                }
+            }
+        }
+    }
+}
--- Editor/SpatialAwarenessPackageModulesInstaller.cs
+++ Editor/SpatialAwarenessPackageModulesInstaller.cs
@@ -4,19 +4,21 @@
 using RealityCollective.ServiceFramework.Definitions;
 using RealityCollective.ServiceFramework.Editor.Packages;
 using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.SpatialAwareness.Definitions;
+using RealityToolkit.SpatialAwareness.Interfaces.SpatialObservers;
+using System.Linq;
 using UnityEditor;
 
 namespace RealityToolkit.SpatialAwareness.Editor
 {
     /// <summary>
-    /// Installs <see cref="Ispatial_awarenessModule"/>s coming from a third party package
-    /// into the <see cref="spatial_awarenessProfile"/> in the <see cref="ServiceManager.ActiveProfile"/>.
+    /// Installs the spatial awareness package modules in the <see cref="ServiceManager.ActiveProfile"/>.
     /// </summary>
     [InitializeOnLoad]
     public sealed class SpatialAwarenessPackageModulesInstaller : IPackageModulesInstaller
     {
         /// <summary>
-        /// Static initializer for the installer instance.
+        /// Statis initalizer for the installer instance.
         /// </summary>
         static SpatialAwarenessPackageModulesInstaller()
         {
@@ -36,57 +38,44 @@ namespace RealityToolkit.SpatialAwareness.Editor
         /// <inheritdoc/>
         public bool Install(ServiceConfiguration serviceConfiguration)
         {
-            /*
-            -------------------------------------------------------
-            TO install modules for the service, uncomment the code below.
+            if (!typeof(ISpatialAwarenessServiceModule).IsAssignableFrom(serviceConfiguration.InstancedType.Type))
+            {
+                // This module installer does not accept the configuration type.
+                return false;
+            }
 
-            Note, in order to correctly assign modules for the service, you need to replace the following (Where spatial_awareness is the service name used to generate this repository):
+            if (!ServiceManager.IsActiveAndInitialized)
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} is not initialized.");
+                return false;
+            }
 
-            - spatial_awareness with the correct service type.
-            - spatial_awarenessModule with the correct module type.
-            - Ispatial_awarenessModule with the correct module interface.
-            - spatial_awarenessProfile with the correct profile type.
+            if (!ServiceManager.Instance.HasActiveProfile)
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} has no active profile.");
+                return false;
+            }
 
-            These are collated from the service and module definitions generated using the Service Template Generator
-            -------------------------------------------------------
-                        if (!typeof(Ispatial_awarenessModule).IsAssignableFrom(serviceConfiguration.InstancedType.Type))
-                        {
-                            // This module installer does not accept the configuration type.
-                            return false;
-                        }
+            if (!ServiceManager.Instance.TryGetServiceProfile<ISpatialAwarenessService, SpatialAwarenessSystemProfile>(out var spatialAwarenessServiceProfile))
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(SpatialAwarenessSystemProfile)} not found.");
+                return false;
+            }
 
-                        if (!ServiceManager.IsActiveAndInitialized)
-                        {
-                            UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} is not initialized.");
-                            return false;
-                        }
+            // Setup the configuration.
+            var typedServiceConfiguration = new ServiceConfiguration<ISpatialAwarenessServiceModule>(serviceConfiguration.InstancedType.Type, serviceConfiguration.Name, serviceConfiguration.Priority, serviceConfiguration.RuntimePlatforms, serviceConfiguration.Profile);
 
-                        if (!ServiceManager.Instance.HasActiveProfile)
-                        {
-                            UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} has no active profile.");
-                            return false;
-                        }
+            // Make sure it's not already in the target profile.
+            if (spatialAwarenessServiceProfile.ServiceConfigurations.All(sc => sc.InstancedType.Type != serviceConfiguration.InstancedType.Type))
+            {
+                spatialAwarenessServiceProfile.AddConfiguration(typedServiceConfiguration);
+                UnityEngine.Debug.Log($"Successfully installed the {serviceConfiguration.InstancedType.Type.Name} to {spatialAwarenessServiceProfile.name}.");
+            }
+            else
+            {
+                UnityEngine.Debug.Log($"Skipped installing the {serviceConfiguration.InstancedType.Type.Name} to {spatialAwarenessServiceProfile.name}. Already installed.");
+            }
 
-                        if (!ServiceManager.Instance.TryGetServiceProfile<Ispatial_awareness, spatial_awarenessProfile>(out var spatial_awarenessProfile))
-                        {
-                            UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(spatial_awarenessProfile)} not found.");
-                            return false;
-                        }
-
-                        // Setup the configuration.
-                        var typedServiceConfiguration = new ServiceConfiguration<Ispatial_awarenessModule>(serviceConfiguration.InstancedType.Type, serviceConfiguration.Name, serviceConfiguration.Priority, serviceConfiguration.RuntimePlatforms, serviceConfiguration.Profile);
-
-                        // Make sure it is not already in the target profile.
-                        if (spatial_awarenessProfile.ServiceConfigurations.All(sc => sc.InstancedType.Type != serviceConfiguration.InstancedType.Type))
-                        {
-                            spatial_awarenessProfile.AddConfiguration(typedServiceConfiguration);
-                            UnityEngine.Debug.Log($"Successfully installed the {serviceConfiguration.InstancedType.Type.Name} to {spatial_awarenessProfile.name}.");
-                        }
-                        else
-                        {
-                            UnityEngine.Debug.Log($"Skipped installing the {serviceConfiguration.InstancedType.Type.Name} to {spatial_awarenessProfile.name}. Already installed.");
-                        }
-            */
             return true;
         }
     }
